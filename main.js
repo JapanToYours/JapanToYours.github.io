5e38dd65b2901ed51c909f645c641ee772e7f72c
JapanToYours <jptoyou2021@gmail.com>
2021-11-01 03:16:22 +0800

    1

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function convert() {
   var jpyShipping = Number(document.getElementById("jpyShipping").value);
   var weight = Number(document.getElementById("weight").value);
   var shippingRate = Number(document.getElementById("shippingRate").value);
-  sellPrice.innerHTML = (jpyPrice + jpyShipping)*(jpyRate+0.06) + weight*shippingRate + 200;
+  var fee = Number(document.getElementById("fee").value);
+  sellPrice.innerHTML = (jpyPrice + jpyShipping)*(jpyRate+0.06) + weight*shippingRate + fee;
   ourPrice.innerHTML = (jpyPrice + jpyShipping)*(jpyRate+0.03) + weight*(shippingRate+180);
   return
 }
