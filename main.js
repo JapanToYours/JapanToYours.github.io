4e794c7f7316dab46ca7dc30ad958fdedf1d192d
JapanToYours <j2u.shop@gmail.com>
2021-11-13 18:02:01 +0800

    1

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function convert() {
   var sellPrice = document.getElementById("sellPrice");
   var ourPrice = document.getElementById("ourPrice");
+  var customerPrice = document.getElementById("customerPrice");
+
   var jpyPrice = Number(document.getElementById("jpyPrice").value);
   var jpyRate = Number(document.getElementById("jpyRate").value);
   var jpyShipping = Number(document.getElementById("jpyShipping").value);
@@ -8,8 +10,11 @@ function convert() {
   var shippingRate = Number(document.getElementById("shippingRate").value);
   var clothNum = Number(document.getElementById("clothNum").value);
   var fee = Number(document.getElementById("fee").value);
+  var customerFee = Number(document.getElementById("customerFee").value);
   var method = Number(document.getElementById("method").value);
+
   sellPrice.innerHTML = ((jpyPrice + jpyShipping)*(jpyRate+0.06) + weight*shippingRate + fee + (clothNum-1)*100)*method;
   ourPrice.innerHTML = ((jpyPrice + jpyShipping)*(jpyRate+0.03) + weight*(shippingRate+80))*method;
+  customerPrice.innerHTML = (jpyPrice + jpyShipping)*(jpyRate+0.06) + customerFee
   return
 }
