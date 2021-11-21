21dcedc4495010431a3756ff13dc450acf9b4241
JapanToYours <j2u.shop@gmail.com>
2021-11-21 23:30:53 +0800

    123

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ function Convert() {
   if (method == 1) {
     if (weight < 1) { weight = 1};
 
-    notice.innerHTML = "代購商品僅尾款(200元上下)走蝦皮<br>其餘先匯款或是刷卡<br>也可先收訂金每一萬日幣收一千台幣起"
     highestPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06) + (weight+0.2)*(shippingRate) + shopee*0.06 + 100 + (pieces-1)*50;
     lowestPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.03) + (weight+0.1)*(shippingRate) + shopee*0.06 + 60 + (pieces-1)*30;
     sellingPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06) + serviceFee;
@@ -42,7 +41,6 @@ function Convert() {
     else if (method == 2) {
       if (weight < 1) { weight = 1};
 
-      notice.innerHTML = "預購商品全額蝦皮<br>記得調整蝦皮賣場價格至全額"
       highestPrice.value = ((jpyPrice + jpyShipping)*(jpyRate+0.06) + (weight+0.2)*(shippingRate))*1.1 + shopee*0.1 + 200;
       lowestPrice.value = ((jpyPrice + jpyShipping)*(jpyRate+0.03) + (weight+0.1)*(shippingRate))*1.05 + shopee*0.1 + 100;
       sellingPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06) + serviceFee;
@@ -52,7 +50,6 @@ function Convert() {
       else if (sellingPrice.value > highestPrice.value) {alert("報價高於頂價 可能偏貴 請注意")};
   }
     else if (method == 3) {
-      notice.innerHTML = "小物(200g以下)不單獨易利空運<br>需跟其他商品一起走國際段否則算是代購"
       highestPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06)*1.1 + weight*shippingRate + shopee*0.06 + 100 + (pieces-1)*50;
       lowestPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.03)*1.05 + shopee*0.06 + 100 + (pieces-1)*30;
       sellingPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06) + serviceFee;
@@ -62,7 +59,6 @@ function Convert() {
       else if (sellingPrice.value > highestPrice.value) {alert("報價高於頂價 可能偏貴 請注意")};
   }
     else if (method == 4) {
-      notice.innerHTML = "庫存現貨不得超過五萬"
       highestPrice.value = ((jpyPrice + jpyShipping)*(jpyRate+0.06) + (weight+0.1)*(shippingRate))*3 + shopee*0.06 + 100;
       lowestPrice.value = ((jpyPrice + jpyShipping)*(jpyRate+0.03) + (weight)*(shippingRate))*1.5 + shopee*0.06 + 60;
       sellingPrice.value = (jpyPrice + jpyShipping)*(jpyRate+0.06) + serviceFee;
@@ -79,10 +75,11 @@ function Convert() {
   sellingPrice.innerHTML = sellingPrice.value;
   cost.innerHTML = cost.value;
   return
-}
+};
 
 function Pieces() {
   var pieces = Number(document.getElementById("pieces").value);
+
   for (var i = 1; i < pieces; i++) {
     document.getElementById("jpyPrice"+(i+1).toString()).type = "number";
   };
@@ -90,4 +87,22 @@ function Pieces() {
     document.getElementById("jpyPrice"+i.toString()).type = "hidden";
     document.getElementById("jpyPrice"+i.toString()).value = 0;
   };
-}
+  return
+};
+
+function Method() {
+  var method = Number(document.getElementById("method").value)
+
+  if (method == 1) {
+    notice.innerHTML = "代購商品僅尾款(200元上下)走蝦皮<br>其餘先匯款或是刷卡<br>也可先收訂金每一萬日幣收一千台幣起";
+  }
+  else if (method == 2) {
+    notice.innerHTML = "預購商品全額蝦皮<br>記得調整蝦皮賣場價格至全額";
+  }
+  else if (method == 3) {
+    notice.innerHTML = "小物(200g以下)不單獨易利空運<br>需跟其他商品一起走國際段否則算是代購";
+  }
+  else if (method == 4) {
+    notice.innerHTML = "庫存現貨不得超過五萬";
+  }
+};
